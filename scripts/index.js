6faa1c2399719dae8c2506f1085d8553b17c0fdc
Taylor Wind <ct_race7393@yahoo.com>
2025-10-21 21:36:08 -0500

    revise functions for opening and closing modals

--- scripts/index.js
+++ scripts/index.js
@@ -36,37 +36,43 @@ const imageCaptionInput = newPostModal.querySelector("#image-caption-input");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+}
 
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
+}
 editProfileBtn.addEventListener("click", function() {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_opened");
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function() {
-  editProfileModal.classList.remove("modal_opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function() {
-  newPostModal.classList.add("modal_opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function() {
-  newPostModal.classList.remove("modal_opened");
+  closeModal(newPostModal);
 });
 
 function handleEditProfileSubmit (evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_opened");
+  closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 function handleNewPostSubmit (evt) {
   evt.preventDefault();
-  newPostModal.classList.remove("modal_opened");
+  closeModal(newPostModal);
   console.log(imageLinkInput.value);
   console.log(imageCaptionInput.value);
 }
